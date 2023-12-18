de0126c554974f7ed7eae760a4aaa952063d8a93
Adiletbaike <adiletdzhuraev@gmail.com>
2023-12-18 18:18:00 +0600

    React Functions

--- src/components/Hotel.jsx
+++ src/components/Hotel.jsx
@@ -1,10 +1,12 @@
 import CreatableSelect from "react-select/creatable";
 import React, { useRef, useState } from "react";
-import { HiOutlineSearch } from "react-icons/hi";
 import { IoMdAdd } from "react-icons/io";
 import { CiEdit } from "react-icons/ci";
 import { RiDeleteBin5Line } from "react-icons/ri";
 import Modal from "./Modals/Modal";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import DialogDelete from "./Modals/DialogDelete";
 
 const hotel = [
   { value: "mekkah", label: "Мекке" },
@@ -30,35 +32,142 @@ const hotels = [
 ];
 
 const Hotel = () => {
-  const nameRef = useRef();
-  const mapLinkRef = useRef();
-  const cityRef = useRef();
+  // Modal
+  const [showModal, setShowModal] = useState(false);
+
   // Table
   const [data, setData] = useState(hotels);
   const [selectedCity, setSelectedCity] = useState(null);
 
-  //handle values
+  // Handle values
+  const nameRef = useRef();
+  const mapLinkRef = useRef();
+  const cityRef = useRef();
   const handleValues = (e) => {
     e.preventDefault();
     const name = nameRef.current.value;
     const mapLink = mapLinkRef.current.value;
-    const city = selectedCity ? selectedCity.label : '';
+    const city = selectedCity ? selectedCity.label : "";
     var newHotel = {
       name,
       mapLink,
       city,
     };
-    console.log(newHotel);
     setData((prevData) => prevData.concat(newHotel));
     nameRef.current.value = "";
     mapLinkRef.current.value = "";
     setSelectedCity(null);
+    setShowModal(false);
+
+    // Show toast notification
+    toast.success("Ийгиликтүү сакталды!!!", {
+      position: "top-right",
+      autoClose: 3000, // 3 seconds
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
   };
 
-  // Modal
-  const [showModal, setShowModal] = useState(false);
+  // Delete
+  const nameHotelRef = useRef();
+  const [dialogDelete, setDialogDelete] = useState({
+    message: "",
+    isLoading: false,
+  });
+  const handleDialog = (message, isLoading) => {
+    setDialogDelete({
+      message,
+      isLoading,
+    });
+  };
+  const handleDelete = (name) => {
+    handleDialog("Чындап өчүрүүнү каалайсызбы?", true);
+    nameHotelRef.current = name;
+  };
+
+  const areYouSureDelete = (choose) => {
+    if (choose) {
+      setData(data.filter((hotel) => hotel.name !== nameHotelRef.current));
+      handleDialog("", false);
+      // Show toast notification
+      toast.error("Ийгиликтүү өчүрүлдү!!!", {
+        position: "top-right",
+        autoClose: 3000, // 3 seconds
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        style: {
+          backgroundColor: "#fff", // Set your desired background color
+        },
+      });
+    } else {
+      handleDialog("", false);
+    }
+  };
+
+  // Edit
+  const [edit, setEdit] = useState(false);
+  const [editData, setEditData] = useState({
+    name: "",
+    mapLink: "",
+    city: "",
+  });
+  const handleEdit = (name) => {
+    setEdit(true);
+    setEditData(data.find((hotel) => hotel.name === name));
+  };
+  const handleEditValues = (e) => {
+    e.preventDefault();
+    const name = nameRef.current.value;
+    const mapLink = mapLinkRef.current.value;
+    const city = selectedCity ? selectedCity.label : "";
+    var editedHotel = {
+      name,
+      mapLink,
+      city,
+    };
+    // Update the data in edit mode
+    setData((prevData) =>
+      prevData.map((hotel) =>
+        hotel.name === editData.name ? editedHotel : hotel
+      )
+    );
+    // Reset edit mode and data
+    setEdit(false);
+    setEditData({
+      name: "",
+      mapLink: "",
+      city: "",
+    });
+
+    // Reset form fields
+    nameRef.current.value = "";
+    mapLinkRef.current.value = "";
+    setSelectedCity(null);
+    setShowModal(false);
+    // Toast notification
+    toast.info("Ийгиликтүү өзгөртүлдү!!!", {
+      position: "top-right",
+      autoClose: 3000, // 3 seconds
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      style: {
+        backgroundColor: "#fff", // Set your desired background color
+      },
+    });
+  };
+
   return (
     <div className="bg-white p-4">
+      <ToastContainer />
       <div className="flex justify-end border-b pb-4 border-gray-200">
         <button
           className="flex items-center text-lg rounded-lg border p-1 bg-green-400"
@@ -72,7 +181,11 @@ const Hotel = () => {
             <h3 className="mb-4 text-xl font-medium text-gray-900">
               Мейманканалар
             </h3>
-            <form className="space-y-3" action="#" onSubmit={handleValues}>
+            <form
+              className="space-y-3"
+              action="#"
+              onSubmit={edit ? handleEditValues : handleValues}
+            >
               <div>
                 <label
                   htmlFor="name"
@@ -87,6 +200,7 @@ const Hotel = () => {
                   id="name"
                   placeholder="Аты"
                   required
+                  defaultValue={edit ? editData.name : ""}
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                 />
               </div>
@@ -104,6 +218,7 @@ const Hotel = () => {
                   id="mapLink"
                   placeholder="Адрес"
                   required
+                  defaultValue={edit ? editData.mapLink : ""}
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                 />
               </div>
@@ -118,6 +233,11 @@ const Hotel = () => {
                   ref={cityRef}
                   onChange={(selectedOption) => setSelectedCity(selectedOption)}
                   isClearable
+                  defaultValue={
+                    edit && editData.city
+                      ? { value: "city", label: editData.city }
+                      : null
+                  }
                   options={hotel}
                 />
               </div>
@@ -126,7 +246,7 @@ const Hotel = () => {
                   type="submit"
                   className="w-100 text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                 >
-                  Сактоо
+                  {edit ? "Өзгөртүү" : "Сактоо"}
                 </button>
               </div>
             </form>
@@ -199,15 +319,27 @@ const Hotel = () => {
                         </div>
                       </td>
                       <td className=" flex px-6 py-6 whitespace-nowrap gap-2 border-none text-right text-2xl items-center font-medium">
-                        <a
-                          href="#"
+                        <button
+                          onClick={() => {
+                            handleEdit(hotel.name);
+                            setShowModal(true);
+                          }}
                           className="text-indigo-600 hover:text-indigo-900"
                         >
                           <CiEdit />
-                        </a>
-                        <a href="#" className="text-red-600 hover:text-red-900">
+                        </button>
+                        <button
+                          onClick={() => handleDelete(hotel.name)}
+                          className="text-red-600 hover:text-red-900"
+                        >
                           <RiDeleteBin5Line />
-                        </a>
+                        </button>
+                        {dialogDelete.isLoading && (
+                          <DialogDelete
+                            onDialog={areYouSureDelete}
+                            message={dialogDelete.message}
+                          />
+                        )}
                       </td>
                     </tr>
                   ))}
