1b7ca1e34d8ff61bf6f1fad1755b9e001cdeff12
Adiletbaike <adiletdzhuraev@gmail.com>
2024-04-14 15:42:01 +0600

    fix

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -46,7 +46,7 @@ const Login = () => {
 
   return (
     <div className="relative">
-      <div className="flex justify-center bg-[url(https://images.unsplash.com/photo-1693590614566-1d3ea9ef32f7?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] h-screen bg-center">
+      <div className="flex justify-center bg-[url(https://images.unsplash.com/photo-1693590614566-1d3ea9ef32f7?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] h-screen bg-center bg-no-repeat">
         <div className="h-[90%] w-full md:w-full pt-20 flex flex-col justify-center items-center">
           <div className="text-xl cursor-pointer flex flex-col justify-center items-center mt-5 md:mt-0">
             <h1 className="font-semibold text-3xl text-gray-700 m-2">Логин</h1>
@@ -77,7 +77,7 @@ const Login = () => {
                 min={0}
                 value={companyId}
                 onChange={(e) => setCompanyId(e.target.value)}
-                placeholder="Compony id"
+                placeholder="Company id"
                 className=" bg-gray-100 rounded-lg px-5 py-2 focus:border border-violet-600 focus:outline-none text-gray-600 placeholder:opacity-50 font-semibold md:w-72 lg:w-[340px]"
               />
             </div>
